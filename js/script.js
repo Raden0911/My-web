21904305c6d01e3020b3bbedfd2ea8dbb4b3ed1f
Raden0911 <vizaycoc@gmail.com>
2023-06-05 11:43:50 +0700

    menambahkan semua file

---
+++ js/script.js
@@ -0,0 +1,8 @@
+// Toggle class active
+// const navbarnav = document.querySelector('.navbar-nav');
+// Hamburger aktif ketika di klik
+// document.querySelector('#hamburger-menu').onclick = () => {
+    // navbarnav.classList.toggle('active');
+// };
+
+
